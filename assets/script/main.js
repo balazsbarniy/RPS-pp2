121f36916082c20040b783911f8e3900b1c4e9ac
Barna <128509430+balazsbarniy@users.noreply.github.com>
2023-07-19 20:05:22 +0100

    updates

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,79 +1,113 @@
-
-
-
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissor_div = document.getElementById("scissor");
-const text_div = document.querySelector(".text");
+const result_div = document.querySelector(".text");
 const user_points = document.getElementById("user-points");
-const sistem_points = document.getElementById("sistem-points")
+const system_points = document.getElementById("system-points");
+const restart_div = document.getElementById("restart-div");
+const restart_button =document.getElementById("restart");
 let user_score = 0;
-let sistem_score = 0;
+let system_score = 0;
 
 function valuation(user) {
-    var sistem = sistemchoose();
-    switch(user + sistem) {
+    var system = systemchoose();
+    switch(user + system) {
         case "rockscissor":
         case "paperrock":
         case "scissorpaper":
-        //console.log("win. us: "+User+"sis: "+sistem);
-        win(user, sistem);
+        win(user, system);
         break;
             case "rockrock":
             case "paperpaper":
             case "scissorscissor":
-            //console.log("draw. us: "+User+"sis: "+sistem);
-            draw(user, sistem);
+            draw(user, system);
             break;
                  case "scissorrock":
                  case "rockpaper":
                  case "paperscissor":
-                 //console.log("lost. us: "+User+"sis: "+sistem);
-                 lost(user, sistem);
+                 lose(user, system);
                  break;
     }
 }
 
 function choose(value){
     if (value === "rock") {
-        valuation("rock")
-        //console.log("ROCK")
+        valuation("rock");
+        
     }
     else if (value === "paper") {
-        valuation("paper")
-        //console.log("PAPER")
+        valuation("paper");
+        
     }
     else if(value === "scissor") {
-        valuation("scissor")
-        //console.log("SCISSOR")
+        valuation("scissor");
+        
     }
-
+load(choose);
 }
-function win(user, sistem) {
+
+
+
+function nameconvert(value) {
+    if (value === "rock") {
+        return "Rock";
+    }
+    else if (value === "paper") {
+        return "Paper";
+    }
+    else  {
+        return "Scissor";
+    }
+}
+function win(user, system) {
     user_score++;
-    text_div.innerHTML = user= " win "+sistem+" lose"
-    user_points.innerHTM = user_score;
-
+    result_div.innerHTML = nameconvert(user) + " win , " + nameconvert(system) + " lose";
+    user_points.innerHTML = user_score;
+    
 }
-function win(user, sistem) {
-    text_div.innerHTML = user= " and "+sistem+" same, draw!!"
+
+function draw(user, system) {
+    result_div.innerHTML = nameconvert(user) + " and  "+ nameconvert(system) + " same, draw!!";
    
 
 }
 
-function lose(user, sistem) {
-    sistem_score++;
-    text_div.innerHTML = user= " lose "+sistem+" win"
-    sistem_points.innerHTM = sistem_score;
+function lose(user, system) {
+    system_score++;
+    result_div.innerHTML = nameconvert(user) + " lose , "+ nameconvert(system) + " win";
+    system_points.innerHTML = system_score;
 
 }
+function scoreCheck() {
+    if (user_score >= 10){
+        result_div.innerHTML = "U WIN!!   to restart click to 'Restart' button!"
+        rock_div.removeEventListener("click", () => choose("rock"));
+        paper_div.removeEventListener("click", () => choose("paper"));
+        scissor_div.removeEventListener("click", () => choose("scissor"));
+        restart_div.classList.remove("invisible");
+        restart_div.classList.add("visible");
+        restart_button.addEventListener("click", () => location.reload());
+    }else if (system_score >=10){
+        result_div.innerHTML = "SYSTEM WIN!!   to restart click to 'Restart' button!"
+        rock_div.removeEventListener("click", () => choose("rock"));
+        paper_div.removeEventListener("click", () => choose("paper"));
+        scissor_div.removeEventListener("click", () => choose("scissor"));
+        restart_div.classList.remove("invisible");
+        restart_div.classList.add("visible");
+        restart_button.addEventListener("click", () => location.reload());
+
+    }
+}
+setInterval(scoreCheck, 100);
 
 
-function sistemchoose() {
+function systemchoose() {
     let number = Math.floor(Math.random() * 3);
     let answer = ["rock", "paper", "scissor"];
     return answer[number];
 }
+
+
 
 function load(){
     rock_div.addEventListener("click",() => choose("rock"));
@@ -81,3 +115,6 @@ function load(){
     scissor_div.addEventListener("click", () => choose("scissor"));
 
 }
+load(rock_div);
+load(paper_div);
+load(scissor_div);
